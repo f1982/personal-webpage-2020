2e6aa600a013a01d0d3822bdfe4e7f551dbaa6ac
Andy Cho <caojundan@gmail.com>
2020-02-02 12:19:40 +1300

    Add homepage to package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { GlobalStyle, myTheme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
