7a44c387fc31ce79830f40e1f080025fe8203dcc
Andy Cao <caojundan@gmail.com>
2020-11-25 19:32:07 +1300

    refactor TechnologyStackItem

--- src/comps/project/TechnologyStack.tsx
+++ src/comps/project/TechnologyStack.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TSItem = styled.span`
-    font-family: Menlo, Monaco, monospace, 'Courier New';
-    padding: 2px 0.5rem;
-    font-size: 0.75rem;
-    background-color: #eee;
-    border: 1px solid #ccc;
-    border-radius: 0.5rem;
-    line-height: 1rem;
-    list-style-type: none;
-    :hover {
-        background: #555;
-    }
+  font-family: Menlo, Monaco, monospace, 'Courier New';
+  padding: 2px 0.5rem;
+  font-size: 0.75rem;
+  background-color: #eee;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  line-height: 1rem;
+  list-style-type: none;
+  :hover {
+    background: #555;
+  }
 `;
 
 interface TechnologyStackItemProp {
-    technology: string;
+  technology: string;
 }
 
-const TechnologyStackItem = (props: TechnologyStackItemProp) => {
-    return (
-        <TSItem>
-            <small>{props.technology}</small>
-        </TSItem>
-    );
+const TechnologyStackItem = ({ technology }: TechnologyStackItemProp) => {
+  return (
+    <TSItem>
+      <small>{technology}</small>
+    </TSItem>
+  );
 };
 
 export { TechnologyStackItem };
