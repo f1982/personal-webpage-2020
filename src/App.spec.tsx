917719f9f464712b79856924cdf2cffd0e29a30c
Andy Cao <caojundan@gmail.com>
2020-12-13 18:39:29 +1300

    refactor(app): follow the eslint rules

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,53 +1,53 @@
-import React from 'react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
-import AppView from './AppView';
+import React from 'react'
+import { createMemoryHistory } from 'history'
+import { Router } from 'react-router-dom'
+import AppView from './AppView'
 
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react'
 it('test if show welcome page by default', () => {
-    const history = createMemoryHistory();
-    const { getByText } = render(
-        <Router history={history}>
-            <AppView />
-        </Router>
-    );
+  const history = createMemoryHistory()
+  const { getByText } = render(
+    <Router history={history}>
+      <AppView />
+    </Router>
+  )
 
-    //   expect(screen.getByText(/Welcome to my space/i)).toBeInTheDocument();
+  //   expect(screen.getByText(/Welcome to my space/i)).toBeInTheDocument()
 
-    const greeting = getByText("Hey! I'm Andy");
-    //   expect(greeting).toBeInTheDocument();
-    //   expect(greeting.textContent).toEqual("Hey! I'm Andy");
+  // const greeting = getByText("Hey! I'm Andy")
+  // expect(greeting).toBeInTheDocument()
+  //   expect(greeting.textContent).toEqual("Hey! I'm Andy")
 
-    //   const button = getByText('ENTER');
-    //   fireEvent.click(button);
+  //   const button = getByText('ENTER')
+  //   fireEvent.click(button)
 
-    //   const hi = getByText('Hi there,');
-    //   expect(hi.textContent).toEqual('Hi there,');
-});
+  //   const hi = getByText('Hi there,')
+  //   expect(hi.textContent).toEqual('Hi there,')
+})
 
 // it('test if show welcome page by default', () => {
 //   const wrapper = mount(
 //     <MemoryRouter>
 //       <AppView />
 //     </MemoryRouter>
-//   );
+//   )
 
-//   expect(wrapper.find(Welcome)).toHaveLength(1);
-//   expect(wrapper.find(PageNotFound)).toHaveLength(0);
-// });
+//   expect(wrapper.find(Welcome)).toHaveLength(1)
+//   expect(wrapper.find(PageNotFound)).toHaveLength(0)
+// })
 
 // it('test page not found', () => {
 //   const wrapper = mount(
 //     <MemoryRouter initialEntries={['/404']}>
 //       <AppView />
 //     </MemoryRouter>
-//   );
+//   )
 
-//   console.log('found: ', wrapper.find(Welcome));
-//   expect(wrapper.find(Welcome)).toHaveLength(1);
-//   expect(wrapper.find(PageNotFound)).toHaveLength(1);
+//   console.log('found: ', wrapper.find(Welcome))
+//   expect(wrapper.find(Welcome)).toHaveLength(1)
+//   expect(wrapper.find(PageNotFound)).toHaveLength(1)
 
-//   //   console.log(wrapper.find(Home));
-//   //   expect(wrapper.find(Welcome)).toHaveLength(1);
-//   //   expect(wrapper.find(Home)).toHaveLength(1);
-// });
+//   //   console.log(wrapper.find(Home))
+//   //   expect(wrapper.find(Welcome)).toHaveLength(1)
+//   //   expect(wrapper.find(Home)).toHaveLength(1)
+// })
--- src/pages/contact/comps/LocationMap.tsx
+++ src/pages/contact/comps/LocationMap.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import GoogleMap, { Coords } from 'google-map-react';
+import React from 'react'
+import GoogleMap, { Coords } from 'google-map-react'
 
-const YOUR_GOOGLE_MAP_API_KEY = 'AIzaSyDRAHe23jG9GXgLKUTsayaxaLiEFWKg8-k';
+const YOUR_GOOGLE_MAP_API_KEY = 'AIzaSyDRAHe23jG9GXgLKUTsayaxaLiEFWKg8-k'
 
 const MapFlag = (props: any) => {
-    return <span style={{ fontSize: `2rem` }}>{props.text}</span>;
-};
-
-interface MapPropType {
-    center: Coords;
-    zoom: Number;
-    greatPlaceCoords: Coords;
+  return <span style={{ fontSize: `2rem` }}>{props.text}</span>
 }
 
-let defaultCenter: Coords = { lat: -36.802647, lng: 174.735249 };
-const CurrentMap = (props: any) => {
-    return (
-        <div style={{ height: '50vh', width: '100%', borderRadius: `20px` }}>
-            <GoogleMap
-                bootstrapURLKeys={{ key: YOUR_GOOGLE_MAP_API_KEY }} // set if you need stats etc ...
-                center={defaultCenter}
-                zoom={12}>
-                <MapFlag lat={defaultCenter.lat} lng={defaultCenter.lng} text='📍' />
-                {/* <MapFlag lat={59.824465} lng={30.180121} text='My Marker2' /> */}
-            </GoogleMap>
-        </div>
-    );
-};
+interface MapPropType {
+  center: Coords
+  zoom: Number
+  greatPlaceCoords: Coords
+}
 
-export default CurrentMap;
+const defaultCenter: Coords = { lat: -36.802647, lng: 174.735249 }
+const CurrentMap = () => {
+  return (
+    <div style={{ height: '50vh', width: '100%', borderRadius: `20px` }}>
+      <GoogleMap
+        bootstrapURLKeys={{ key: YOUR_GOOGLE_MAP_API_KEY }} // set if you need stats etc ...
+        center={defaultCenter}
+        zoom={12}>
+        <MapFlag lat={defaultCenter.lat} lng={defaultCenter.lng} text='📍' />
+        {/* <MapFlag lat={59.824465} lng={30.180121} text='My Marker2' /> */}
+      </GoogleMap>
+    </div>
+  )
+}
+
+export default CurrentMap
